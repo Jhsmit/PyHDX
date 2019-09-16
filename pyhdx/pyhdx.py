7eb16b10941d56c2eeba2122b8372d77dc319e48
Jochem Smit <j.h.smit@rug.nl>
2019-09-16 15:49:17 +0200

    changes changed

--- pyhdx/pyhdx.py
+++ pyhdx/pyhdx.py
@@ -4,7 +4,7 @@
 
 import numpy as np
 import itertools
-
+import scipy
 from .math import solve_nnls
 
 CSV_DTYPE = [
@@ -49,7 +49,7 @@ class PeptideCSVFile(object):
 
 
 class PeptideMeasurements(object):
-    def __init__(self, data, scores=None):
+    def __init__(self, data, scores=None, min_len=0):
         """data: structured array with at least the fields 'start', 'end', 'sequence'
 
         """
@@ -58,7 +58,7 @@ class PeptideMeasurements(object):
         self.stop = np.max(self.data['end'])
         self.prot_len = self.stop - self.start + 1
 
-        self.scores = scores if scores is not None else None
+        self.scores = scores if scores is not None else None  # TODO currently needs scores for len
 
         self.big_X = np.zeros((len(data), self.prot_len), dtype=float)
         for row, entry in enumerate(self.data):
@@ -84,18 +84,48 @@ class PeptideMeasurements(object):
         vals, idx, counts = np.unique(big_sum, return_index=True, return_counts=True)
         vals = vals[np.argsort(idx)]  #
         self.counts = counts[np.argsort(idx)]
-        cs = np.cumsum(counts)
+        cs = np.cumsum(self.counts)
+
+        if min_len > 0:
+
+            merged_counts = np.array([], dtype=int)
+            rr_prev = 0
+            for rl, rr in contiguous_regions(self.counts <= 2):
+                merged_counts = np.append(merged_counts, self.counts[rr_prev:rl])
+                rr_prev = rr
+                s = self.counts[rl:rr].sum()
+                merged_counts = np.append(merged_counts, s)
+
+            final_counts = merged_counts.copy()
+            for i, e in enumerate(merged_counts):
+                if e <= 2:
+                    final_counts[i] -= e
+                    if i == 0:
+                        final_counts[i + 1] += e
+                    elif i == len(merged_counts) - 1:
+                        final_counts[i - 1] += e
+                    elif final_counts[i + 1] < final_counts[i - 1]:
+                        final_counts[i + 1] += e
+                    else:
+                        final_counts[i - 1] += e
+            final_counts = final_counts[final_counts > 0]
+            assert final_counts.sum() == self.counts.sum()
+            print(final_counts)
+            self.counts = final_counts
+            cs = np.cumsum(self.counts)
 
 
+        print(cs)
         # Matrix of N columns with N equal to sections of identical residues.
         # Values are the number of residues in the blocks
-        self.X = np.zeros((len(self.scores), len(vals)), dtype=float)
+        self.X = np.zeros((len(data), len(self.counts)), dtype=float)
         for row, entry in enumerate(data):
             i0 = entry['start'] - self.start
             i1 = entry['end'] - self.start + 1
             p = np.searchsorted(cs, [i0, i1], side='right')
+            print(row, p, i0, i1)
 
-            self.X[row][p[0]:p[1]] = counts[p[0]:p[1]]
+            self.X[row][p[0]:p[1]] = self.counts[p[0]:p[1]]
 
     @property
     def X_norm(self):
@@ -122,6 +152,40 @@ class PeptideMeasurements(object):
         x = solve_nnls(self.X_norm.T, self.scores, reg=reg)
         return np.repeat(x, self.counts)
 
+    def scores_nnls(self):
+        print(np.any(np.isnan(self.X_norm)))
+        print(np.any(np.isinf(self.X_norm)))
+        x = scipy.optimize.nnls(self.X_norm, self.scores,)[0]
+        return x
+
     def calc_scores(self, residue_scores):
         return self.big_X.dot(residue_scores)
 
+
+
+
+#https://stackoverflow.com/questions/4494404/find-large-number-of-consecutive-values-fulfilling-condition-in-a-numpy-array
+def contiguous_regions(condition):
+    """Finds contiguous True regions of the boolean array "condition". Returns
+    a 2D array where the first column is the start index of the region and the
+    second column is the end index."""
+
+    # Find the indicies of changes in "condition"
+    d = np.diff(condition)
+    idx, = d.nonzero()
+
+    # We need to start things after the change in "condition". Therefore,
+    # we'll shift the index by 1 to the right.
+    idx += 1
+
+    if condition[0]:
+        # If the start of condition is True prepend a 0
+        idx = np.r_[0, idx]
+
+    if condition[-1]:
+        # If the end of condition is True, append the length of the array
+        idx = np.r_[idx, condition.size] # Edit
+
+    # Reshape the result into two columns
+    idx.shape = (-1,2)
+    return idx
