789e5ac8360c9c21e00548059032e3163fee1226
Jochem Smit <jhsmit@gmail.com>
2020-02-10 16:49:17 +0100

    added docstrings to fitting, removed expfact files

--- pyhdx/fitting.py
+++ pyhdx/fitting.py
@@ -5,7 +5,7 @@ from symfit.core.minimizers import DifferentialEvolution, Powell
 from collections import namedtuple
 
 
-#module level parameters are likely to cause all sort of problems
+#module level parameters are likely to cause all sorts of problems
 r = Parameter('r', value=0.5, min=0, max=1)
 tau1 = Parameter('tau1', min=0, max=5)
 tau2 = Parameter('tau2', min=0, max=100)
@@ -15,14 +15,70 @@ model = Model({y: 100 *(1 - (r*exp(-t/tau1) + (1-r)*exp(-t/tau2)))})
 
 
 def func_short(tau, tt, A):
+    """
+    Function to estimate the short time component
+
+    Parameters
+    ----------
+    tau : :obj:`float`
+        Lifetime
+    tt : :obj:`float`
+        Selected time point
+    A : :obj:`float`
+        Target amplitude
+
+    Returns
+    -------
+    A_t : :obj:`float`
+        Amplitude difference given tau, tt, A
+
+    """
     return 100 * (1 - np.exp(-tt / tau)) - A
 
 
 def func_long(tau, tt, A, tau1):
+    """
+    Function to estimate the short time component
+
+    Parameters
+    ----------
+    tau : :obj:`float`
+        Lifetime
+    tt : :obj:`float`
+        Selected time point
+    A : :obj:`float`
+        Target amplitude
+    tau1: : obj:`float`
+        Lifetime of short time component
+
+    Returns
+    -------
+    A_t : :obj:`float`
+        Amplitude difference given tau, tt, A, tau1
+
+    """
     return 100 * (1 - (0.5 * np.exp(-tt / tau1) + 0.5 * np.exp(-tt / tau))) - A
 
 
 def initial_guess(t, d):
+    """
+    Calculates initial guesses for fitting of two-component kinetic uptake reaction
+
+    Parameters
+    ----------
+    t : :class:~`numpy.ndarray`
+        Array with time points
+    d : :class:~`numpy.ndarray`
+        Array with uptake values
+
+    Returns
+    -------
+    tau1 : :obj:`float`
+        Initial guess for short time component
+    tau2 : :obj:`float`
+        Initial guess for long time component
+
+    """
     tau1 = fsolve(func_short, 2, args=(t[2], d[2]))[0]
     tau2 = fsolve(func_long, 20, args=(t[-2], d[-2], tau1))[0]
 
@@ -34,6 +90,23 @@ er = EmptyResult(np.nan, {k: np.nan for k in ['tau1', 'tau2', 'r']})
 
 
 def fit_kinetics(t, d, chisq_thd):
+    """
+    Fit time kinetics with two time components and corresponding relative amplitude.
+
+    Parameters
+    ----------
+    t : :class:`~numpy.ndarray`
+        Array of time points
+    d : :class:`~numpy.ndarray`
+        Array of uptake values
+    chisq_thd: :obj:`float`
+        Threshold chi squared above which the fitting is repeated with the Differential Evolution algorithm.
+
+    Returns
+    -------
+    res : :class:`~symfit.FitResults`
+        Symfit fitresults object.
+    """
     if np.any(np.isnan(d)):  # states!
         return er
 
