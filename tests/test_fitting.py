124ca3683be7ec9c5892b4172cb9a664c91610cf
Jochem Smit <jhsmit@gmail.com>
2020-04-24 11:58:33 +0200

    fixed and updating fiting test

--- tests/test_fitting.py
+++ tests/test_fitting.py
@@ -3,9 +3,11 @@ import os
 from pyhdx import PeptideMeasurements, PeptideCSVFile, KineticsSeries
 from pyhdx.fileIO import read_dynamx
 from pyhdx.fitting import KineticsFitting, KineticsFitResult
+from pyhdx.support import fmt_export, np_from_txt
 import numpy as np
 
 directory = os.path.dirname(__file__)
+np.random.seed(43)
 
 
 class TestFitting(object):
@@ -13,29 +15,35 @@ class TestFitting(object):
     def test_fit_section(self):
         fpath = os.path.join(directory, 'test_data', 'ds1.csv')
         control_100 = ('PpiA-FD', 0.167)
+        control_100 = ('PpiANative', 30.000002)
+
         state = 'PpiANative'
 
         data = read_dynamx(fpath)
+        print(np.unique(data['exposure']))
         pcf = PeptideCSVFile(data)
-        b = pcf.data['start'] < 50
-        pcf.data = pcf.data[b]
+        #b = pcf.data['start'] < 50
+        #pcf.data = pcf.data[b]
 
         pcf.set_control(control_100)
         states = pcf.groupby_state()
         series = states[state]
 
         kf = KineticsFitting(series)
-        fr = kf.weighted_avg_fit()
+        fr1 = kf.weighted_avg_fit()
 
-        rate = fr.rate
-        expected = np.array([1.00335533, 1.00335533, 1.00335533, 1.00335533, 1.00335533, 1.00335533,
-                             1.00335533, 1.00335533, 0.65508649, 0.63973347, 0.70839084, 0.70839084,
-                             0.70839084, 0.70839084, 0.70839084, 0.66683659, 0.60067197, 0.43035482,
-                             0.33043168, 0.30382436, 0.29411784, 0.27600381, 0.27600381, 0.27600381,
-                             0.27600381, 0.27600381, 0.27600381, 0.27600381, 0.27600381, 0.27600381,
-                             0.27600381, 0.27418457, 0.26622289, 0.26622289, 0.26759417, 0.27282912,
-                             0.27282912, 0.27282912, 0.27282912, 0.27282912, 0.23271286, 0.23271286,
-                             0.23271286, 0.23271286, 0.23271286, 0.23271286, 0.21722117, 0.21722117,
-                             0.21722117, 0.21722117, 0.21722117, 0.21722117, 0.21722117, 0.21722117])
+        arr1 = fr1.get_output(['rate', 'tau', 'tau1', 'tau2', 'r'])
 
-        np.testing.assert_allclose(expected, rate, rtol=1e-7)
+        gt = np_from_txt(os.path.join(directory, 'test_data', 'fit1_result.txt'))
+        for name in ['rate', 'tau', 'tau1', 'tau2', 'r']:
+            np.testing.assert_allclose(arr1[name], gt[name], rtol=1e-2)
+
+
+        fr2 = kf.lsq_fit_blocks(arr1)
+        arr2 = fr2.get_output(['rate', 'tau', 'tau1', 'tau2', 'r'])
+
+        gt = np_from_txt(os.path.join(directory, 'test_data', 'fit2_result.txt'))
+        for name in ['rate', 'tau', 'tau1', 'tau2', 'r']:
+            np.testing.assert_allclose(arr2[name], gt[name], rtol=1e-2)
+
+
