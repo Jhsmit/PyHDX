e03f8db944c22e366941eb495dfd660f14c921a8
Jochem Smit <jhsmit@gmail.com>
2020-04-24 11:57:22 +0200

    fix bug where fitting didnt use the correct data

--- pyhdx/fitting.py
+++ pyhdx/fitting.py
@@ -352,8 +352,9 @@ class KineticsFitting(object):
         models = []
         intervals = [] # Intervals; (start, end); (inclusive, exclusive)
 
-        for series in self.k_series.split().values():
-            arr = self.scores_stack.T  # todo use nonnormed scores as they should be normalized already
+        for k, series in self.k_series.split().items():
+            arr = series.scores_stack.T
+            #arr = self.scores_stack.T  # todo use nonnormed scores as they should be normalized already
             i = 0
             for bl in series.cov.block_length:
                 intervals.append((series.cov.start + i, series.cov.start + i + bl + 1))
