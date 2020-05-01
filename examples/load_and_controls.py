b92683cc676bf277b826ffd26d87d7ef3a5f0d2f
Jochem Smit <jhsmit@gmail.com>
2020-05-01 16:52:55 +0200

    updated example

--- examples/load_and_controls.py
+++ examples/load_and_controls.py
@@ -1,5 +1,7 @@
 import pyhdx
 from pyhdx.fitting import KineticsFitting
+from pyhdx.support import np_from_txt
+import numpy as np
 
 filename = r"..\tests\test_data\ds1.csv"
 drop_first = 1  # Number of N terminal residues to ignore
@@ -14,6 +16,12 @@ pf.set_control(control_100)
 states = pf.groupby_state()
 series = states[series_name]
 
+
 kf = KineticsFitting(series)
 
 fit_result = kf.weighted_avg_fit()
+fr1 = kf.weighted_avg_fit()
+arr1 = fr1.get_output(['rate', 'tau', 'tau1', 'tau2', 'r'])
+
+fr2 = kf.lsq_fit_blocks(arr1)
+arr2 = fr2.get_output(['rate', 'tau', 'tau1', 'tau2', 'r'])
