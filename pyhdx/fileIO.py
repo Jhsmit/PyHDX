f08b246709bc28c510b5a5c50337abb179c14a7d
Jochem Smit <jhsmit@gmail.com>
2020-06-18 13:06:01 +0200

    cleanup and docs

--- pyhdx/fileIO.py
+++ pyhdx/fileIO.py
@@ -3,38 +3,19 @@ from numpy.lib.recfunctions import stack_arrays
 from io import StringIO
 
 
-# CSV_DTYPE = [
-#     ('Protein', 'U', 'c'),
-#     ('start', 'i', 'i'),
-#     ('end', 'i', 'i'),
-#     ('sequence', 'U', 'c'),
-#     ('modification', 'U', 'c'),
-#     ('fragment', 'U', 'c'),
-#     ('max_uptake', 'i', 'i'),
-#     ('MHP', 'f', 'f'),
-#     ('state', 'U', 'c'),
-#     ('exposure', 'f', 'f'),
-#     ('center', 'f', 'f'),
-#     ('center_sd', 'f', 'f'),
-#     ('uptake', 'f', 'f'),
-#     ('uptake_sd', 'f', 'f'),
-#     ('RT', 'f', 'f'),
-#     ('RT_sd', 'f', 'f')
-# ]
-
 def read_dynamx(*file_paths):
     """
     Reads a dynamX .csv file and returns the data as a numpy structured array
 
     Parameters
     ----------
-    file_path: :obj:`str`
+    file_paths: :obj:`iterable`
         File path of the .csv file or StringIO object
 
     Returns
     -------
     data : :class:`~numpy.ndarray`
-        numpy structured array with
+        Peptides as a numpy structured array
 
     """
 
@@ -55,44 +36,3 @@ def read_dynamx(*file_paths):
     full_data = stack_arrays(data_list, usemask=True, autoconvert=True)
     return full_data
 
-
-def write_expfact(data_list, name):
-    keyfunc = lambda x: x.exposure
-    data_list = sorted(data_list, key=keyfunc)
-    check_data(data_list)
-
-    pm = data_list[0]
-    write_dexp(data_list, name + '.Dexp')
-    write_seq(pm, name + '.seq')
-    write_ass(pm, name + '.list')
-
-
-def check_data(data_list):
-    c = data_list[0]
-    for d in data_list[1:]:
-        for field in ['start', 'end', 'sequence']:
-            assert np.all(c.data[field] == d.data[field])
-
-
-def write_dexp(data_list, fname):
-    dexp = np.array([pm.scores / 100 for pm in data_list])
-    times = np.array([v.exposure for v in data_list])
-    out = np.insert(dexp, 0, times / 60, axis=1)
-
-    np.savetxt(fname, out)
-
-
-def write_ass(pm, fname):
-    new_data = np.empty(len(pm), dtype= [('index', int), ('start', int), ('end', int), ('sequence', '<U64')])
-    new_data['index'] = np.arange(len(pm)) + 1
-    new_data['start'] = pm.data['start']
-    new_data['end'] = pm.data['end']
-    new_data['sequence'] = pm.data['sequence']
-
-    #data = np.vstack([np.arange(len(pm)) + 1] + [pm.data[name] for name in ['start', 'end', 'sequence']])
-    np.savetxt(fname, new_data, fmt='%i %i %i %10s')
-
-
-def write_seq(pm, fname):
-    with open(fname, 'w') as f:
-        f.write(pm.sequence)
