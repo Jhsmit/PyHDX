92ef307ee1d7e11383c8d78d9ecfb018adb5f102
Jochem Smit <jhsmit@gmail.com>
2020-07-14 12:22:22 +0200

    added pbr versioning

--- pyhdx/__init__.py
+++ pyhdx/__init__.py
@@ -2,11 +2,13 @@
 
 """Top-level package for PyHDX."""
 
-__author__ = """Jochem Smit"""
-__email__ = 'jhsmit@gmail.com'
-__version__ = '0.1.0'
-
 from .pyhdx import PeptideMasterTable, PeptideMeasurements, KineticsSeries, Coverage
 from .fitting import KineticsFitting
 from .fileIO import read_dynamx
 from .output import Output
+
+
+from pbr.version import VersionInfo
+package_name = 'pyhdx'
+info = VersionInfo(package_name)
+__version__ = info.version_string()
--- setup.py
+++ setup.py
@@ -17,32 +17,35 @@ setup_requirements = ['pytest-runner', ]
 
 test_requirements = ['pytest', ]
 
-setup(
-    author="Jochem Smit",
-    author_email='jhsmit@gmail.com',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ],
-    description="PyHDX",
-    entry_points={
-        'console_scripts': [
-            'pyhdx=pyhdx.cli:main',
-        ],
-    },
-    install_requires=requirements,
-    long_description=readme + '\n\n' + history,
-    include_package_data=True,
-    keywords='pyhdx',
-    name='pyhdx',
-    packages=find_packages(include=['pyhdx']),
-    setup_requires=setup_requirements,
-    test_suite='tests',
-    tests_require=test_requirements,
-    url='https://github.com/Jhsmit/pyhdx',
-    version='0.1.0',
-    zip_safe=False,
-)
+#setup_requires=['pbr>1.9', 'setuptools>17.1'],
+setup(pbr=True)
+
+# setup(
+#     author="Jochem Smit",
+#     author_email='jhsmit@gmail.com',
+#     classifiers=[
+#         'Development Status :: 2 - Pre-Alpha',
+#         'Intended Audience :: Developers',
+#         'Natural Language :: English',
+#         'Programming Language :: Python :: 3.6',
+#         'Programming Language :: Python :: 3.7',
+#     ],
+#     description="PyHDX",
+#     entry_points={
+#         'console_scripts': [
+#             'pyhdx=pyhdx.cli:main',
+#         ],
+#     },
+#     install_requires=requirements,
+#     long_description=readme + '\n\n' + history,
+#     include_package_data=True,
+#     keywords='pyhdx',
+#     name='pyhdx',
+#     packages=find_packages(include=['pyhdx']),
+#     setup_requires=setup_requirements,
+#     test_suite='tests',
+#     tests_require=test_requirements,
+#     url='https://github.com/Jhsmit/pyhdx',
+#     version='0.1.0',
+#     zip_safe=False,
+# )
