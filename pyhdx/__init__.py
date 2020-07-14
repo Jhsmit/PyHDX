76ae5d2f1395d01284530f63a5fd767d52c87bf4
Jochem Smit <jhsmit@gmail.com>
2020-07-14 15:31:38 +0200

    updated versioning, manually supplying git dir

--- pyhdx/__init__.py
+++ pyhdx/__init__.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 """Top-level package for PyHDX."""
 
 from .pyhdx import PeptideMasterTable, PeptideMeasurements, KineticsSeries, Coverage
@@ -7,23 +5,31 @@ from .fitting import KineticsFitting
 from .fileIO import read_dynamx
 from .output import Output
 
-from pbr.version import VersionInfo
+from pbr import version
 from pbr import git
 from pkg_resources import DistributionNotFound
 import os
 from pathlib import Path
 
+
 package_name = 'pyhdx'
 try:
-    info = VersionInfo(package_name)
+    info = version.VersionInfo(package_name)
     __version__ = info.version_string()
     __dev_version__ = info.version_string_with_vcs()
+    __git_sha__ = git.get_git_short_sha()
 
-except:
-    __version__ = '0.0.0'
-    __dev_version__ = '0.0.0'
-    print('Warning: Version number not found.')
 
-__git_sha__ = git.get_git_short_sha()
+except Exception:  # Pbr throws very broad Exception, for some reason DistributionNotFound does not want to be caught
+    git_dir = Path(__file__).parent.parent / '.git'
+    tagged = git._run_git_command(
+        ['describe'], git_dir,
+        throw_on_error=True).replace('-', '.')
+    semantic_version = version.SemanticVersion.from_pip_string(tagged)
+    __version__ = semantic_version.brief_string()
+    __dev_version__ = semantic_version._long_version(None)
+    __git_sha__ = git.get_git_short_sha(git_dir)
+
+
 git_str = f' ({__git_sha__})' if __git_sha__ else ''
 VERSION_STRING = f'pyHDX version {__version__}, development version {__dev_version__}' + git_str
