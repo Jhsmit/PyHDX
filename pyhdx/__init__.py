a2ea2ea4ddd9ed3b28f96a03f0bebacd17ed46a4
Jochem Smit <jhsmit@gmail.com>
2020-07-14 15:35:41 +0200

    try except for if git is not found

--- pyhdx/__init__.py
+++ pyhdx/__init__.py
@@ -22,14 +22,19 @@ try:
 
 except Exception:  # Pbr throws very broad Exception, for some reason DistributionNotFound does not want to be caught
     git_dir = Path(__file__).parent.parent / '.git'
-    tagged = git._run_git_command(
-        ['describe'], git_dir,
-        throw_on_error=True).replace('-', '.')
-    semantic_version = version.SemanticVersion.from_pip_string(tagged)
-    __version__ = semantic_version.brief_string()
-    __dev_version__ = semantic_version._long_version(None)
-    __git_sha__ = git.get_git_short_sha(git_dir)
-
+    try:
+        tagged = git._run_git_command(
+            ['describe'], git_dir,
+            throw_on_error=True).replace('-', '.')
+        semantic_version = version.SemanticVersion.from_pip_string(tagged)
+        __version__ = semantic_version.brief_string()
+        __dev_version__ = semantic_version._long_version(None)
+        __git_sha__ = git.get_git_short_sha(git_dir)
+    except FileNotFoundError:
+        print('git not installed')
+        __version__ = '0.0.0'
+        __dev_version__ = '0.0.0'
+        __git_sha__ = ''
 
 git_str = f' ({__git_sha__})' if __git_sha__ else ''
 VERSION_STRING = f'pyHDX version {__version__}, development version {__dev_version__}' + git_str
