ce49872e379e31df1e55c42b026645a461b41e38
Jochem Smit <jhsmit@gmail.com>
2020-08-03 19:28:33 +0200

    added script for local cluster

--- pyhdx/local_cluster.py
+++ pyhdx/local_cluster.py
@@ -12,22 +12,21 @@ os.chdir(Path(__file__).parent.parent)
 from pyhdx.math import solve_nnls
 
 
-#
-# if __name__ == '__main__':
-#
-#
-#     cluster = LocalCluster(scheduler_port=52123, n_workers=10)
-#     print(cluster)
-#
-#     try:
-#         loop = True
-#         while loop:
-#             try:
-#                 time.sleep(2)
-#                 print(cluster)
-#             except KeyboardInterrupt:
-#                 print('interrupted')
-#                 loop = False
-#     finally:
-#         print('closing')
-#         cluster.close()
+if __name__ == '__main__':
+
+
+    cluster = LocalCluster(scheduler_port=52123, n_workers=10)
+    print(cluster)
+
+    try:
+        loop = True
+        while loop:
+            try:
+                time.sleep(2)
+                print(cluster)
+            except KeyboardInterrupt:
+                print('interrupted')
+                loop = False
+    finally:
+        print('closing')
+        cluster.close()
