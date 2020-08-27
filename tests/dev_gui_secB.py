16551461faa9568f33854a890aba8739f647ba50
Jochem Smit <jhsmit@gmail.com>
2020-08-27 13:32:32 +0200

    reload GUI secB updates

--- tests/dev_gui_secB.py
+++ tests/dev_gui_secB.py
@@ -18,7 +18,14 @@ dic['norm_state'] = 'Full deuteration control'
 dic['norm_exposure'] = 0.167
 dic['exp_state'] = 'SecB WT apo'
 
+src_file = os.path.join(directory, 'test_data', 'SecB WT apo_pfact_linear.txt')
+array = np_from_txt(src_file)
+src_dict = {name: array[name] for name in array.dtype.names}
+src_dict['y'] = src_dict['log_P']
+src_dict['color'] = np.full_like(array, fill_value=DEFAULT_COLORS['pfact'], dtype='<U7')
 
+dic['sources'] = {}
+dic['sources']['pfact'] = src_dict
 
 cluster = None
 
