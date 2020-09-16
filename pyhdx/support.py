2acb806f39088297df750f838fb62cda397f8d9e
Jochem Smit <jhsmit@gmail.com>
2020-09-16 14:02:34 +0200

    fixed bug in reloading files with color fields (comment tag)

--- pyhdx/support.py
+++ pyhdx/support.py
@@ -251,7 +251,8 @@ def np_from_txt(file_path, delimiter='\t'):
     # else:
     #     names = None
 
-    return np.genfromtxt(file_path, dtype=None, names=names, skip_header=1, delimiter=delimiter, encoding=None, autostrip=True)
+    return np.genfromtxt(file_obj, dtype=None, names=names, skip_header=header_lines, delimiter=delimiter,
+                         encoding=None, autostrip=True, comments=None)
 
 
 def try_wrap(coverage, wrap, margin=4):
