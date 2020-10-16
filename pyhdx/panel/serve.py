7c5ea2d5692775bf2bfd7a73a47e8a0e73f52110
Jochem Smit <jhsmit@gmail.com>
2020-10-16 15:09:17 +0200

    added folding app to serve

--- pyhdx/panel/serve.py
+++ pyhdx/panel/serve.py
@@ -1,10 +1,11 @@
 import panel as pn
-from pyhdx.panel.apps import main_app, diff_app, single_app
+from pyhdx.panel.apps import main_app, diff_app, single_app, folding_app
 
 APP_DICT = {
     'main': main_app,
     'single': single_app,
-    'diff': diff_app
+    'diff': diff_app,
+    'folding': folding_app
 }
 
 
