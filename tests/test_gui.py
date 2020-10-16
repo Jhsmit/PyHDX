472d036314d8eb42cc76392b259cef92d55acc13
Jochem Smit <jhsmit@gmail.com>
2020-10-16 12:14:09 +0200

    updated tests for GUI

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -5,7 +5,7 @@ from pathlib import Path
 directory = Path(__file__).parent
 
 
-class TestGui(object):
+class TestMainGUISecB(object):
     @classmethod
     def setup_class(cls):
         cls.fpath = directory / 'test_data' / 'ecSecB_apo.csv'
@@ -17,7 +17,6 @@ class TestGui(object):
 
         states = cls.pmt.groupby_state()
         cls.series = states[cls.state]
-        cls.series.make_uniform()
 
     def test_load_files(self):
         with open(self.fpath, 'rb') as f:
@@ -51,6 +50,67 @@ class TestGui(object):
 
         ctrl.control_panels['InitialGuessControl']._action_fit()
         assert 'half-life' in ctrl.sources.keys()
+
+
+class TestMainGUISimulated(object):
+    @classmethod
+    def setup_class(cls):
+        cls.fpath = directory / 'test_data' / 'simulated_data_uptake.csv'
+        cls.pmt = PeptideMasterTable(read_dynamx(cls.fpath))
+
+        cls.state = 'state1'
+        cls.pmt.set_backexchange(0.)
+
+        states = cls.pmt.groupby_state()
+        cls.series = states[cls.state]
+
+    def test_load_files(self):
+        with open(self.fpath, 'rb') as f:
+            binary = f.read()
+
+        tmpl, ctrl = _main_app()
+        file_input_control = ctrl.control_panels['PeptideFileInputControl']
+
+        file_input_control.file_selectors[0].value = binary
+        file_input_control._action_load()
+        file_input_control.norm_mode = 'Theory'
+        file_input_control.be_percent = 0.
+
+        file_input_control.exp_state = self.state
+        assert file_input_control.exp_exposures == [0.167, 0.5, 1.0, 5.0, 10.0, 30., 100.]
+        file_input_control._action_parse()
+
+        assert isinstance(ctrl.series, KineticsSeries)
+        assert isinstance(ctrl.peptides, PeptideMasterTable)
+
+    def test_coverage(self):
+        tmpl, ctrl = _main_app()
+        ctrl.series = self.series
+
+    def test_initial_guesses(self):
+        tmpl, ctrl = _main_app()
+        ctrl.cluster = None
+        ctrl.series = self.series
+
+        initial_guess = ctrl.control_panels['InitialGuessControl']
+        initial_guess._action_fit()
+        assert 'half-life' in ctrl.sources.keys()
+
+        initial_guess.fitting_model = 'Association'
+        initial_guess._action_fit()
+        assert 'fit1' in ctrl.sources.keys()
+
+    def test_tf_fit(self):
+        tmpl, ctrl = _main_app()
+        ctrl.cluster = None
+        ctrl.series = self.series
+
+        initial_guess = ctrl.control_panels['InitialGuessControl']
+        initial_guess._action_fit()
+
+        fit = ctrl.control_panels['FitControl']
+        fit._do_fitting()
+        assert 'pfact' in ctrl.sources.keys()
 
 
 class TestDiffApp(object):
