c11cddf68903c080fb17910d9940acaf447da812
Jochem Smit <jhsmit@gmail.com>
2020-11-06 13:47:02 +0100

    added file export tests

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -1,8 +1,19 @@
 from pyhdx import PeptideMasterTable, read_dynamx, KineticsSeries
+from pyhdx.fileIO import txt_to_protein, txt_to_np
 from pyhdx.panel.apps import _main_app, _diff_app
+from pyhdx.panel.data_sources import DataSource
 from pathlib import Path
 
+import numpy as np
+
 directory = Path(__file__).parent
+
+
+TEST_PML = """set_color color_#0a0ac2, [10,10,194]
+set_color color_#8c8c8c, [140,140,140]
+color color_#0a0ac2, resi 10-17 + resi 19-25 + resi 27-28 + resi 30-37 + resi 39-57 + resi 62-84 + resi 86-94 + resi 100-102 + resi 104-107 + resi 109-113 + resi 115-123 + resi 125-129 + resi 131-133 + resi 138-155
+color color_#8c8c8c, resi 18-18 + resi 26-26 + resi 29-29 + resi 38-38 + resi 58-61 + resi 85-85 + resi 95-99 + resi 103-103 + resi 108-108 + resi 114-114 + resi 124-124 + resi 130-130 + resi 134-137
+"""
 
 
 class TestMainGUISecB(object):
@@ -17,6 +28,11 @@ class TestMainGUISecB(object):
 
         states = cls.pmt.groupby_state()
         cls.series = states[cls.state]
+
+        cls.prot_fit_result = txt_to_protein(directory / 'test_data' / 'ecSecB_torch_fit.txt')
+
+        cls.ds_fit = DataSource(cls.prot_fit_result, name='global_fit', x='r_number', tags=['mapping', 'pfact', 'deltaG'],
+                                renderer='circle', size=10)
 
     def test_load_files(self):
         with open(self.fpath, 'rb') as f:
@@ -58,7 +74,29 @@ class TestMainGUISecB(object):
 
         fit_control = ctrl.control_panels['FitControl']
         fit_control.epochs = 10
-        fit_control.do_fit()
+        fit_control._do_fitting()
+
+        deltaG_figure = ctrl.figure_panels['DeltaGFigure']
+        renderer = deltaG_figure.figure.renderers[0]
+        assert renderer.data_source.name == 'global_fit'
+
+    def test_file_download_output(self):
+        tmpl, ctrl = _main_app()
+        ctrl.series = self.series
+        ctrl.publish_data('global_fit', self.ds_fit)
+
+        f_export = ctrl.control_panels['FileExportControl']
+        f_export.target = 'global_fit'
+        pml = f_export._make_pml('sadf')
+
+        assert pml == TEST_PML
+
+        io = f_export.linear_export_callback()
+        val = io.getvalue()
+
+        assert val.count('\n') == 148
+        array = txt_to_np(io)
+        assert np.allclose(f_export.export_dict['deltaG'], array['deltaG'], equal_nan=True)
 
 
 class TestMainGUISimulated(object):
