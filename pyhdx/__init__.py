4d0cfd97a9ec6aa73b2efeb0a6ed97204bd69445
Jochem Smit <jhsmit@gmail.com>
2021-01-12 22:10:59 +0100

    importlib for versions

--- pyhdx/__init__.py
+++ pyhdx/__init__.py
@@ -5,34 +5,41 @@ from .fitting import KineticsFitting
 from .fileIO import read_dynamx
 from .output import Output
 
-from pbr import version
-from pbr import git
-from pathlib import Path
+# from pbr import version
+# from pbr import git
+# from pathlib import Path
 
 package_name = 'pyhdx'
-try:
-    info = version.VersionInfo(package_name)
-    __version__ = info.version_string()
-    __dev_version__ = info.version_string_with_vcs()
-    __git_sha__ = git.get_git_short_sha()
+
+import importlib.metadata
+__version__ = importlib.metadata.version(package_name)
 
 
-except Exception:  # Pbr throws very broad Exception, for some reason DistributionNotFound does not want to be caught
-    git_dir = Path(__file__).parent.parent / '.git'
-    try:
-        tagged = git._run_git_command(
-            ['describe', '--tags'], git_dir,
-            throw_on_error=True).replace('-', '.')
-        semantic_version = version.SemanticVersion.from_pip_string(tagged)
-        __version__ = semantic_version.brief_string()
-        __dev_version__ = semantic_version._long_version(None)
-        __git_sha__ = git.get_git_short_sha(git_dir)
-    except FileNotFoundError:
-        print('git not installed')
-        __version__ = '0.0.0'
-        __dev_version__ = '0.0.0'
-        __git_sha__ = ''
-
-git_str = f' ({__git_sha__})' if __git_sha__ else ''
-VERSION_STRING = f'pyHDX version {__version__}, development version {__dev_version__}' + git_str
-VERSION_STRING_SHORT = f'pyHDX v{__version__} ({__dev_version__}, {git_str})'
+# print(__version__)
+#
+# try:
+#     info = version.VersionInfo(package_name)
+#     __version__ = info.version_string()
+#     __dev_version__ = info.version_string_with_vcs()
+#     __git_sha__ = git.get_git_short_sha()
+#
+#
+# except Exception:  # Pbr throws very broad Exception, for some reason DistributionNotFound does not want to be caught
+#     git_dir = Path(__file__).parent.parent / '.git'
+#     try:
+#         tagged = git._run_git_command(
+#             ['describe', '--tags'], git_dir,
+#             throw_on_error=True).replace('-', '.')
+#         semantic_version = version.SemanticVersion.from_pip_string(tagged)
+#         __version__ = semantic_version.brief_string()
+#         __dev_version__ = semantic_version._long_version(None)
+#         __git_sha__ = git.get_git_short_sha(git_dir)
+#     except FileNotFoundError:
+#         print('git not installed')
+#         __version__ = '0.0.0'
+#         __dev_version__ = '0.0.0'
+#         __git_sha__ = ''
+#
+# git_str = f' ({__git_sha__})' if __git_sha__ else ''
+# VERSION_STRING = f'pyHDX version {__version__}, development version {__dev_version__}' + git_str
+# VERSION_STRING_SHORT = f'pyHDX v{__version__} ({__dev_version__}, {git_str})'
