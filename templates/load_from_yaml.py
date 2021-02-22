fb174cc643912774bdfd237e233e0fa03561909c
Jochem Smit <jhsmit@gmail.com>
2021-02-22 11:54:39 +0100

    template safe load yaml

--- templates/load_from_yaml.py
+++ templates/load_from_yaml.py
@@ -8,12 +8,7 @@ data_dir = root_dir.parent / 'tests' / 'test_data'
 
 print(data_dir)
 yaml_file_path = Path('SecB.yaml')
-SecB_dic = yaml.load(yaml_file_path.read_text())
+SecB_dic = yaml.safe_load(yaml_file_path.read_text())
 
 kf = load_from_yaml(SecB_dic, data_dir=data_dir)
 print(kf)
-
-#
-# fit_dic = yaml.load(Path('batch_fit_config.yaml').read_text())
-# fit_dic['epochs'] = 10
-# fit_result = do_fitting_from_yaml()
