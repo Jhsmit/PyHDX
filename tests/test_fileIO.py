9d880a3a1efe638c8ac4cdff5fa0159b888212e5
sajetan <sajetnayaran@gmail.com>
2021-03-09 19:49:27 +0530

    some tests for fileIO

--- tests/test_fileIO.py
+++ tests/test_fileIO.py
@@ -1,8 +1,9 @@
 from pyhdx import PeptideMasterTable, read_dynamx, KineticsSeries
-from pyhdx.fileIO import txt_to_protein, txt_to_np, read_dynamx
+from pyhdx.fileIO import txt_to_protein, txt_to_np, read_dynamx, fmt_export, _get_f_width
 from pyhdx.panel.apps import _main_app, _diff_app
 from pyhdx.panel.data_sources import DataSource
 from pathlib import Path
+from io import StringIO
 import pytest
 
 directory = Path(__file__).parent
@@ -27,4 +28,27 @@ class TestFileIO(object):
         assert data['end'][0] == 17
 
         with pytest.raises(ValueError):
-            data = read_dynamx(self.fpath, intervals=('foo', 'bar'))
+            data = read_dynamx(self.fpath, intervals=('foo', 'inclusive'))
+        with pytest.raises(ValueError):
+            data = read_dynamx(self.fpath, intervals=('inclusive', 'bar'))
+
+        with open(self.fpath, mode='r') as f:
+            read_dynamx(StringIO(f.read()))
+
+
+    def test_methods(self):
+        data = read_dynamx(self.fpath)
+        path = directory / 'test_data' / 'ecSecB_guess.txt';
+
+        #testing fmt_export
+        with pytest.raises(ValueError):
+            fmt_export(data, delimiter=',', width="foo")
+
+        #testing txt_to_protein
+        txt_to_protein(path)
+
+        #testing txt_to_np
+        with open(path, mode='r') as f:
+            txt_to_np(StringIO('f'))
+
+
