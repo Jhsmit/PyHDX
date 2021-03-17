c19c730098e76f67ccd6439a3210564b9534fbe4
Jochem Smit <jhsmit@gmail.com>
2021-03-17 18:38:49 +0100

    covariances for batch fitting

--- pyhdx/fitting_torch.py
+++ pyhdx/fitting_torch.py
@@ -27,6 +27,35 @@ class DeltaGFit(nn.Module):
         return t.matmul(X, uptake)
 
 
+def estimate_errors(kf, deltaG):
+    # boolean array to select residues which are exchanging (ie no nterminal resiudes, no prolines, no regions without coverage)
+    bools = kf.series.cov['exchanges'].to_numpy()
+    r_number = kf.series.cov.r_number[bools]  # Residue number which exchange
+    dtype = t.float64
+    temperature = t.tensor([kf.temperature], dtype=dtype)
+    X = t.tensor(kf.series.cov.X[:, bools], dtype=dtype)  # Np x Nr, non-exchanging residues removed
+    k_int = t.tensor(kf.series.cov['k_int'][bools].to_numpy(), dtype=dtype).unsqueeze(-1)  # Nr x 1
+    timepoints = t.tensor(kf.series.timepoints, dtype=dtype).unsqueeze(0)  # 1 x Nt
+
+    deltaG = t.tensor(deltaG[bools], dtype=dtype)
+    output_data = t.tensor(kf.series.uptake_corrected.T, dtype=dtype)
+
+    def calc_loss(deltaG_input):
+        criterion = t.nn.MSELoss(reduction='sum')
+        pfact = t.exp(deltaG_input.unsqueeze(-1) / (constants.R * temperature))
+        uptake = 1 - t.exp(-t.matmul((k_int / (1 + pfact)), timepoints))
+        output = t.matmul(X, uptake)
+
+        loss = criterion(output, output_data)
+        return loss
+
+    hessian = t.autograd.functional.hessian(calc_loss, deltaG)
+    hessian_inverse = t.inverse(-hessian)
+    covariance = np.sqrt(np.abs(np.diagonal(hessian_inverse)))
+
+    #todo return pd series?
+    return Protein({'covariance': covariance, 'r_number': r_number}, index='r_number')
+
 class TorchFitResult(object):
     def __init__(self, fit_object, model, **metadata):
         self.fit_object = fit_object
@@ -73,37 +102,6 @@ class TorchSingleFitResult(TorchFitResult):
     def temperature(self):
         return self.fit_object.temperature
 
-    def estimate_errors(self):
-        # boolean array to select residues which are exchanging (ie no nterminal resiudes, no prolines, no regions without coverage)
-        bools = self.series.cov['exchanges'].to_numpy()
-        r_number = self.series.cov.r_number[bools]  # Residue number which exchange
-        dtype = t.float64
-        temperature = t.tensor([self.temperature], dtype=dtype)
-        X = t.tensor(self.series.cov.X[:, bools], dtype=dtype)  # Np x Nr, non-exchanging residues removed
-        k_int = t.tensor(self.series.cov['k_int'][bools].to_numpy(), dtype=dtype).unsqueeze(-1)  # Nr x 1
-        timepoints = t.tensor(self.series.timepoints, dtype=dtype).unsqueeze(0)  # 1 x Nt
-
-        deltaG = t.tensor(self.deltaG[bools], dtype=dtype)
-        output_data = t.tensor(self.series.uptake_corrected.T, dtype=dtype)
-
-        def calc_loss(deltaG_input):
-            criterion = t.nn.MSELoss(reduction='sum')
-            pfact = t.exp(deltaG_input.unsqueeze(-1) / (constants.R * temperature))
-            uptake = 1 - t.exp(-t.matmul((k_int / (1 + pfact)), timepoints))
-            output = t.matmul(X, uptake)
-
-            loss = criterion(output, output_data)
-            return loss
-
-        hessian = t.autograd.functional.hessian(calc_loss, deltaG)
-        hessian_inverse = t.inverse(-hessian)
-        covariance = np.sqrt(np.abs(np.diagonal(hessian_inverse)))
-
-        return Protein({'covariance': covariance, 'r_number': r_number}, index='r_number')
-
-
-
-
     @property
     def output(self):
         out_dict = {}
@@ -119,7 +117,7 @@ class TorchSingleFitResult(TorchFitResult):
         #todo add possibility to add append series to protein?
         #todo update order of columns
         protein = Protein(out_dict, index='r_number')
-        protein_cov = self.estimate_errors()
+        protein_cov = estimate_errors(self.fit_object, self.deltaG)
         protein = protein.join(protein_cov)
         return protein
 
@@ -157,7 +155,17 @@ class TorchBatchFitResult(TorchFitResult):
 
         output_data[:, 0::len(quantities)] = g_values.T
         output_data[:, 1::len(quantities)] = g_values_nan.T
-        #todo covariances
+
+        for i, kf in enumerate(self.fit_object.states):
+            cov = estimate_errors(kf, g_values[i])
+            pd_series = cov['covariance']
+            print(pd_series)
+
+            pd_series = pd_series.reindex(self.fit_object.r_number)
+            print(pd_series)
+
+            output_data[:, 2+i*len(quantities)] = pd_series.to_numpy()
+
         output_data[:, 3::len(quantities)] = pfact.T
 
         df = pd.DataFrame(output_data, index=self.fit_object.r_number, columns=col_index)
