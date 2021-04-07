9ab030ef0a5b0942bbfb6bd0ccf309d3ad4faa37
Jochem Smit <jhsmit@gmail.com>
2021-04-07 10:27:14 +0200

    updated dev secb file to load two states

--- tests/dev_gui_secB.py
+++ tests/dev_gui_secB.py
@@ -19,35 +19,59 @@ directory = Path(__file__).parent
 fpath_1 = directory / 'test_data' / 'ecSecB_apo.csv'
 fpath_2 = directory / 'test_data' / 'ecSecB_dimer.csv'
 
-dic = {}
-dic['file_paths'] = [fpath_1, fpath_2]
-dic['norm_mode'] = 'Exp'
-dic['fd_state'] = 'Full deuteration control'
-dic['fd_exposure'] = 0.167
-dic['exp_state'] = 'SecB WT apo'
+fpaths = [fpath_1, fpath_2]
+files = [p.read_bytes() for p in fpaths]
 
-# src_file = directory / 'test_data' / 'ecSecB_torch_fit.txt'
-# df = csv_to_dataframe(src_file)
-# data_dict = df.to_dict(orient='series')
-#
-#
-# data_dict['color'] = np.full_like(data_dict['r_number'], fill_value=DEFAULT_COLORS['pfact'], dtype='<U7')
-# data_source = DataSource(data_dict, x='r_number', tags=['mapping', 'pfact', 'deltaG'],
-#                          renderer='circle', size=10, name='global_fit')
-#
-# dic['sources'] = {}
-# #dic['sources']['global_fit'] = data_source  #todo: on_load!
-#
-# dic['rcsb_id'] = '1qyn'
-#
-# cluster = None
-ctrl = reload_previous(dic, ctrl)
-# ctrl.cluster = None
-#
-#
-#ctrl.control_panels['FitControl'].epochs = 10
-#ctrl.cluster = '127.0.0.1:61461'
+file_input = ctrl.control_panels['PeptideFileInputControl']
+
+file_input.input_files = files
+
+file_input.fd_state = 'Full deuteration control'
+file_input.fd_exposure = 0.167
+
+file_input.exp_state = 'SecB WT apo'
+file_input._action_add_dataset()
+file_input.exp_state = 'SecB his dimer apo'
+file_input._action_add_dataset()
 
 
+
+
+
+#
+#
+#
+#
+#
+# dic = {}
+# dic['file_paths'] = [fpath_1, fpath_2]
+# dic['norm_mode'] = 'Exp'
+# dic['fd_state'] = 'Full deuteration control'
+# dic['fd_exposure'] = 0.167
+# dic['exp_state'] = 'SecB WT apo'
+#
+# # src_file = directory / 'test_data' / 'ecSecB_torch_fit.txt'
+# # df = csv_to_dataframe(src_file)
+# # data_dict = df.to_dict(orient='series')
+# #
+# #
+# # data_dict['color'] = np.full_like(data_dict['r_number'], fill_value=DEFAULT_COLORS['pfact'], dtype='<U7')
+# # data_source = DataSource(data_dict, x='r_number', tags=['mapping', 'pfact', 'deltaG'],
+# #                          renderer='circle', size=10, name='global_fit')
+# #
+# # dic['sources'] = {}
+# # #dic['sources']['global_fit'] = data_source  #todo: on_load!
+# #
+# # dic['rcsb_id'] = '1qyn'
+# #
+# # cluster = None
+# ctrl = reload_previous(dic, ctrl)
+# # ctrl.cluster = None
+# #
+# #
+# #ctrl.control_panels['FitControl'].epochs = 10
+# #ctrl.cluster = '127.0.0.1:61461'
+#
+#
 if __name__ == '__main__':
     pn.serve(ctrl.template, show=False, static_dirs={'pyhdx': STATIC_DIR})
