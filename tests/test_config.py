9c7bc2a1a6c8655390091b5055fcab83c5228605
Jochem Smit <jhsmit@gmail.com>
2021-05-03 12:43:55 +0200

    removed unused imports and whitelines

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,12 +1,9 @@
-from pyhdx.fileIO import csv_to_protein, txt_to_np, read_dynamx, fmt_export, csv_to_np
-from pyhdx.models import Protein
 from pathlib import Path
 from pyhdx.panel.configurations import ConfigurationSettings
-from io import StringIO
-import numpy as np
 import pytest
 
 directory = Path(__file__).parent
+
 
 class TestConfig(object):
 
