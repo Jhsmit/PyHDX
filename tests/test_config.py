60295dbc86e5646b70ac932bc046f91481c0543c
Jochem Smit <jhsmit@gmail.com>
2021-05-03 16:32:56 +0200

    add tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,22 +1,33 @@
 from pathlib import Path
-from pyhdx.panel.config import ConfigurationSettings
+from pyhdx.panel.config import ConfigurationSettings, read_config, config_file_path, reset_config
 import pytest
 
 directory = Path(__file__).parent
 
 
 class TestConfig(object):
+    def test_cfg_singleton(self):
+        cfg = ConfigurationSettings()
+        cluster = '127.0.0.1:00000'
+        cfg.cluster = cluster
 
-    def test_update_cluster(self):
-        cluster = ('127.0.0.1', 52125)
-        with pytest.raises(TypeError):
-            ConfigurationSettings().update_cluster(*cluster)
+        assert cfg.get('cluster', 'port') == '00000'
 
-        cluster = ('127.0.0.1', '52125')
-        ConfigurationSettings().update_cluster(*cluster)
+        cfg2 = ConfigurationSettings()
+        assert id(cfg) == id(cfg2)
 
-        set_value = ConfigurationSettings().load_cluster()
-        assert set_value == ':'.join(cluster)
+        cluster = 'asdfsadf'
+        with pytest.raises(ValueError):
+            cfg.cluster = cluster
+
+        cfg.write_config()
+
+        cp_config = read_config(config_file_path)
+        assert cp_config['cluster']['port'] == '00000'
+
+        reset_config()
+        cp_config = read_config(config_file_path)
+        assert cp_config['cluster']['port'] == '52123'
 
 
 
