6c905b3f33b225ac97187a549e19d38a954ccb25
Jochem Smit <jhsmit@gmail.com>
2021-05-20 19:09:03 +0200

    update alignment fucntion

--- pyhdx/alignment.py
+++ pyhdx/alignment.py
@@ -34,7 +34,7 @@ def parse_clustal_string(s, num_proteins, whitelines=2, offset=0):
     return alignment
 
 
-def align_dataframes(dataframes, alignment):
+def align_dataframes(dataframes, alignment, first_r_numbers=None):
     """
     Aligned dataframes based on an alignment.
 
@@ -47,7 +47,9 @@ def align_dataframes(dataframes, alignment):
         List of input dataframes
     alignment : :obj:`list` or :obj:`dict`
         Alignment as list or dict, values are strings with single-letter amino acids codes where gaps are '-'.
-
+    first_r_numbers: :obj:`list`
+        List of residue numbers corresponding to the first residue in the alignment sequences. Use for N-terminally
+        truncated proteins or proteins with fused purification tags.
     Returns
     -------
 
@@ -56,16 +58,32 @@ def align_dataframes(dataframes, alignment):
         multiindex dataframe.
 
     """
+    assert len(alignment) == len(dataframes), "Length of dataframes and alignments does not match"
+
     if isinstance(alignment, dict):
+        if not isinstance(dataframes, dict):
+            raise TypeError("'alignment' and 'dataframes' must either both be `dict` or `list`")
         align_list = list(alignment.values())
-    else:
+        df_list = list(dataframes.values)
+        assert alignment.keys() == dataframes.keys(), "Keys of input dicts to not match"
+    elif isinstance(alignment, list):
+        if not isinstance(alignment, list):
+            raise TypeError("'alignment' and 'dataframes' must either both be `dict` or `list`")
         align_list = alignment
+        df_list = dataframes
+    else:
+        raise TypeError("Invalid data type for 'alignment'")
+
+    first_r_numbers = first_r_numbers if first_r_numbers is not None else [1]*len(dataframes)
+    assert len(first_r_numbers) == len(df_list), "Length of first residue number list does not match number of dataframes"
+
 
     dfs = []
-    for df, align in zip(dataframes, align_list):
+    for df, align, r_offset in zip(df_list, align_list, first_r_numbers):
         align_array = np.array(list(align))
         r_number = np.cumsum(align_array != '-').astype(float)
         r_number[align_array == '-'] = np.nan
+        r_number += r_offset - 1
         index = pd.Index(r_number, name='r_number')
 
         result = df.reindex(index).reset_index().astype({'r_number': 'Int32'})
