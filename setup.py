f294af5f9cc5e61cf453e2889574c16a382078cf
Jochem Smit <jhsmit@gmail.com>
2021-07-14 09:53:31 +0200

    update manifest

--- setup.py
+++ setup.py
@@ -3,17 +3,8 @@
 from setuptools import setup
 import versioneer
 
-
 test_requirements = ['pytest', ]
 
-#setup_requires=['pbr>1.9', 'setuptools>17.1'],
 setup(
     version=versioneer.get_version(),
-    packages=["pyhdx"],
-    entry_points={
-         "console_scripts": [
-             "pyhdx = pyhdx.cli:main",
-
-         ],
-     }
 )
