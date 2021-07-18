4375bb0dbc4a8f2a76e26a4d7765d27aa1b18a65
Jochem Smit <jhsmit@gmail.com>
2021-07-18 13:51:54 +0200

    set versioning back to versioneer

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup, find_packages
 import versioneer
 
 setup(
-    #version=versioneer.get_version(),
-    version='v0.4.0-rc3',
+    version=versioneer.get_version(),
+    #version='v0.4.0-rc3',
     packages=find_packages(),
     include_package_data=True
 )
