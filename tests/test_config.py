815d7bfb5466e08b32ccb6a5fed5481a53b27a8b
Jochem Smit <jhsmit@gmail.com>
2021-08-17 20:32:58 +0200

    updated tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,7 @@
 from pathlib import Path
-from pyhdx.config import ConfigurationSettings, read_config, config_file_path, reset_config
+from pyhdx.config import ConfigurationSettings, read_config, config_file_path, reset_config, write_config
 import pytest
+import tempfile
 
 directory = Path(__file__).parent
 
@@ -8,26 +9,24 @@ directory = Path(__file__).parent
 class TestConfig(object):
     def test_cfg_singleton(self):
         cfg = ConfigurationSettings()
-        cluster = '127.0.0.1:00000'
-        cfg.cluster = cluster
+        scheduler_address = '127.0.0.1:00000'
+        cfg.set('cluster', 'scheduler_address', scheduler_address)
 
-        assert cfg.get('cluster', 'port') == '00000'
+        assert cfg.get('cluster', 'scheduler_address') == scheduler_address
 
         cfg2 = ConfigurationSettings()
         assert id(cfg) == id(cfg2)
 
-        cluster = 'asdfsadf'
-        with pytest.raises(ValueError):
-            cfg.cluster = cluster
+        with tempfile.TemporaryDirectory() as tempdir:
+            fpath = Path(tempdir) / 'config.ini'
+            write_config(fpath, cfg._config)
 
-        cfg.write_config()
+            cp_config = read_config(fpath)
+            assert cp_config['cluster']['scheduler_address'] == '127.0.0.1:00000'
 
+        reset_config()  # the effect of this is currently not tested, needs more extensive tests
         cp_config = read_config(config_file_path)
-        assert cp_config['cluster']['port'] == '00000'
-
-        reset_config()
-        cp_config = read_config(config_file_path)
-        assert cp_config['cluster']['port'] == '52123'
+        assert cp_config['cluster']['scheduler_address'] == '127.0.0.1:52123'
 
 
 
