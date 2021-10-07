befed3dab3b1d5e4b0c481dd4870b75c66e250e4
Jochem Smit <jhsmit@gmail.com>
2021-10-07 13:19:37 +0200

    update fit report template

--- templates/08_fit_report_pdf.py
+++ templates/08_fit_report_pdf.py
@@ -1,13 +1,22 @@
 """Generate a pdf output with all peptide fits. Requires pdflatex"""
-from pyhdx.output import Output, Report
+from pyhdx.output import FitReport
 from pyhdx.fileIO import load_fitresult
 from pathlib import Path
+from concurrent import futures
 
 current_dir = Path().cwd()
-fit_result = load_fitresult(current_dir / 'output' / 'SecB_fit')
+fit_result = load_fitresult(current_dir / 'output' / 'SecB_tetramer_dimer_batch')
 
-output = Output(fit_result)
+tmp_dir = Path(__file__).parent / 'temp'
+tmp_dir.mkdir(exist_ok=True)
 
-report = Report(output)
-report.add_peptide_figures()
-report.generate_pdf(current_dir / 'output' / 'SecB_fit_report')
+if __name__ == '__main__':
+
+    report = FitReport(fit_result, temp_dir=tmp_dir)
+    report.add_peptide_uptake_curves()
+
+    executor = futures.ProcessPoolExecutor(max_workers=10)
+
+    report.generate_figures(executor=executor)
+    report.generate_latex()
+    report.generate_pdf(current_dir / 'pdftest123')
