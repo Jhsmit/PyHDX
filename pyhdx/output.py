be3cdac9073c819649153a67b3b841a2e9fb876e
Jochem Smit <jhsmit@gmail.com>
2021-10-07 13:15:51 +0200

    update fit report, generating figures now in parallel

--- pyhdx/output.py
+++ pyhdx/output.py
@@ -13,14 +13,15 @@ import uuid
 import shutil
 from functools import lru_cache, partial
 from pyhdx.support import grouper, autowrap
-from pyhdx.plot import plot_peptides
 from pyhdx.fitting_torch import TorchSingleFitResult
 from tqdm.auto import tqdm
+from pathlib import Path
 
 import pylatex as pyl
 import proplot as pplt
 import tempfile
 
+from concurrent import futures
 
 geometry_options = {
     "lmargin": "1in",
@@ -28,23 +29,32 @@ geometry_options = {
     }
 
 
-# plot_defaults = {
-#     ''}
+class BaseReport(object):
+    pass
 
 
-class Report(object):
+class Report(BaseReport):
+    def __init__(self, hdxm_set, **kwargs):
+        raise NotImplementedError()
+
+
+class FitReport(object):
     """
-
-    .pdf output document
+    Create .pdf output of a fit result
     """
-
-    def __init__(self, output, title=None, doc=None, add_date=True):
-        self.title = title or f'Fit report for {output.fit_result.data_obj.name}'
-        self.output = output
+    def __init__(self, fit_result, title=None, doc=None, add_date=True, temp_dir=None):
+        self.title = title or f'Fit report'
+        self.fit_result = fit_result
         self.doc = doc or self._init_doc(add_date=add_date)
-        self._temp_dir = self.make_temp_dir()
+        self._temp_dir = temp_dir or self.make_temp_dir()
+        self._temp_dir = Path(self._temp_dir)
+
+        self.figure_queue = []
+        self.tex_dict = {}  # dictionary gathering lists of partial functions which when executed generate the tex output
+        self._figure_number = 0  #todo automate
 
     def make_temp_dir(self):
+        #todo pathlib
         _tmp_path = os.path.abspath(os.path.join(tempfile.gettempdir(), str(id(self))))
 
         if not os.path.exists(_tmp_path):
@@ -53,13 +63,17 @@ class Report(object):
 
     def _init_doc(self, add_date=True):
         doc = pyl.Document(geometry_options=geometry_options)
+        doc.packages.append(pyl.Package('float'))
         doc.packages.append(pyl.Package('hyperref'))
+
         doc.preamble.append(pyl.Command('title', self.title))
         if add_date:
             doc.preamble.append(pyl.Command('date', pyl.NoEscape(r'\today')))
         else:
             doc.preamble.append(pyl.Command('date', pyl.NoEscape(r'')))
         doc.append(pyl.NoEscape(r'\maketitle'))
+        doc.append(pyl.NewPage())
+        doc.append(pyl.Command('tableofcontents'))
         doc.append(pyl.NewPage())
 
         return doc
@@ -70,203 +84,142 @@ class Report(object):
         fig.savefig(filepath, *args, **kwargs)
         return filepath
 
-    def test_mpl(self):
-        fig = plt.figure()
-        plt.plot([2,3,42,1])
-
-        file_path = self._save_fig(fig)
-
-        with self.doc.create(pyl.Figure(position='htbp')) as plot:
-            plot.add_image(pyl.NoEscape(file_path), width=pyl.NoEscape(r'1\textwidth'))
-            plot.add_caption('I am a caption.')
-
-    def add_coverage_figures(self, layout=(6, 2), close=True, **kwargs):
-        raise NotImplementedError()
-        funcs = [partial(self.output._make_coverage_graph, i, **kwargs) for i in range(len(self.output.series))]
-        self.make_subfigure(funcs, layout=layout, close=close)
-
-    def add_peptide_figures(self, ncols=4, nrows=5, **kwargs):
-
-        Np = self.output.fit_result.data_obj.Np
-        indices = range(Np)
-        n = ncols*nrows
-        chunks = [indices[i:i + n] for i in range(0, len(indices), n)]
-        for chunk in tqdm(chunks):
-            with self.doc.create(pyl.Figure(position='ht')) as tex_fig:
-                fig = self.output._make_peptide_subplots(chunk, ncols=ncols, nrows=nrows, **kwargs)
-                file_path = self._save_fig(fig)
-                plt.close(fig)
-
-                tex_fig.add_image(file_path, width=pyl.NoEscape(r'\textwidth'))
-
-        #self.make_subfigure(funcs, layout=layout, close=close)
-
-    def make_subfigure(self, fig_funcs, layout=(5, 4), close=True):
-        #todo figure out how to iterate properly
-        n = np.product(layout)
-        chunks = grouper(n, fig_funcs)
-        w = str(1/layout[1])
-        pbar = tqdm(total=len(fig_funcs))
-        for chunk in chunks:
-            with self.doc.create(pyl.Figure(position='ht')) as tex_fig:
-                for i, fig_func in enumerate(chunk):
-                    if fig_func is None:
-                        continue
-                    with self.doc.create(pyl.SubFigure(position='b', width=pyl.NoEscape(w + r'\linewidth'))) as subfig:
-                        fig = fig_func()
-                        file_path = self._save_fig(fig, bbox_inches='tight') # todo access these kwargs
-                        if close:
-                            plt.close(fig)
-                        subfig.add_image(file_path, width=pyl.NoEscape(r'\linewidth'))
-                    if i % layout[1] == layout[1] - 1:
-                        self.doc.append('\n')
-                    pbar.update(1)
-
-            self.doc.append(pyl.NewPage())
-
-    def test_subfigure(self):
-        fig = plt.figure()
-        plt.plot([2,3,42,1])
-
-        file_path = self._save_fig(fig)
-
-        with self.doc.create(pyl.Figure(position='h!')) as kittens:
-            w = str(0.25)
-            for i in range(8):
-                with self.doc.create(pyl.SubFigure(
-                        position='b',
-                        width=pyl.NoEscape(w + r'\linewidth'))) as left_kitten:
-
-                    left_kitten.add_image(file_path,
-                                          width=pyl.NoEscape(r'\linewidth'))
-                    left_kitten.add_caption(f'Kitten on the {i}')
-                if i % 4 == 3:
-                    self.doc.append('\n')
-            kittens.add_caption("Two kittens")
-
-    def rm_temp_dir(self):
-        """Remove the temporary directory specified in ``_tmp_path``."""
-
-        if os.path.exists(self._temp_dir):
-            shutil.rmtree(self._temp_dir)
-
-    def generate_pdf(self, file_path):
-        self.doc.generate_pdf(file_path, compiler='pdflatex')
-
-
-class Output(object):
-    # Currently only TorchSingleFitResult support
-    def __init__(self, fit_result, time_axis=None, **settings):
-        assert isinstance(fit_result, TorchSingleFitResult), "Invalid type of `fit_result`"
-        self.settings = {'fit_time_axis': 'Log'}
-        self.settings.update(settings)
-
-        #todo restore multiple fit results functionality
-        self.fit_result = fit_result
-        self.fit_timepoints = time_axis or self.get_fit_timepoints()
-        self.d_calc = self.fit_result(self.fit_timepoints)
+    def reset_doc(self, add_date=True):
+        self.doc = self._init_doc(add_date=add_date)
 
     def get_fit_timepoints(self):
-        timepoints = self.fit_result.data_obj.timepoints
-        x_axis_type = self.settings.get('fit_time_axis', 'Log')
+        all_timepoints = np.concatenate([hdxm.timepoints for hdxm in self.fit_result.data_obj])
+
+        #x_axis_type = self.settings.get('fit_time_axis', 'Log')
+        x_axis_type = 'Log' # todo configureable
         num = 100
         if x_axis_type == 'Linear':
-            time = np.linspace(0, timepoints.max(), num=num)
+            time = np.linspace(0, all_timepoints.max(), num=num)
         elif x_axis_type == 'Log':
-            elem = timepoints[np.nonzero(timepoints)]
-            time = np.logspace(np.log10(elem.min()) - 1, np.log10(elem.max()), num=num, endpoint=True)
+            elem = all_timepoints[np.nonzero(all_timepoints)]
+            start = np.log10(elem.min())
+            end = np.log10(elem.max())
+            pad = (end - start)*0.1
+            time = np.logspace(start-pad, end+pad, num=num, endpoint=True)
+        else:
+            raise ValueError("Invalid value for 'x_axis_type'")
 
         return time
 
-    def add_peptide_fits(self, ax_scale='log', fit_names=None):
-        pass
+    def figure_number(self):
+        self._figure_number += 1
+        return self._figure_number
 
-    def peptide_graph_generator(self, **kwargs):
-        for i in range(len(self.series.coverage)):
-            yield from self._make_peptide_graph(i, **kwargs)
+    def add_peptide_uptake_curves(self, layout=(5, 4), time_axis=None):
+        extension = '.pdf'
+        self.tex_dict['peptide_uptake'] = {}
 
-    def _make_peptide_subplots(self, indices, **fig_kwargs):
-        """yield single peptide grpahs"""
-        nrows = fig_kwargs.pop('nrows', int(np.floor(np.sqrt(len(indices)))))
-        ncols = fig_kwargs.pop('ncols', int(np.ceil(len(indices) / nrows)))
+        nrows, ncols = layout
+        n = nrows*ncols
+        time = time_axis or self.get_fit_timepoints()
+        if time.ndim == 1:
+            time = np.tile(time, (len(self.fit_result), 1))
 
-        default_kwargs = {'sharex': 1, 'sharey': 1, 'ncols': ncols, 'nrows': nrows}
-        default_kwargs.update(fig_kwargs)
+        d_calc = self.fit_result(time)  # Ns x Np x Nt
 
-        fig, axes = pplt.subplots(**default_kwargs)
-        axes_iter = iter(axes)
-        for i, ax in zip(indices, axes_iter):
-            ax.plot(self.fit_timepoints, self.d_calc[i], color='r')
-            ax.scatter(self.fit_result.data_obj.timepoints, self.fit_result.data_obj.d_exp.to_numpy()[i], color='k')
+        fig_factory = partial(pplt.subplots, ncols=ncols, nrows=nrows, sharex=1, sharey=1, num=self.figure_number())
 
-            start = self.fit_result.data_obj.coverage.data['_start'][i]
-            end = self.fit_result.data_obj.coverage.data['_end'][i]
-            ax.set_title(f'Peptide_{i}: {start} - {end}')
+        # iterate over samples
+        for hdxm, d_calc_s in zip(self.fit_result.data_obj, d_calc):
+            name = hdxm.name
+            indices = range(hdxm.Np)
+            chunks = [indices[i:i + n] for i in range(0, len(indices), n)]
 
-        t_unit = fig_kwargs.get('time_unit', 'min')
-        t_unit = f'({t_unit})' if t_unit else t_unit
+            tex = []
+            for chunk in chunks:
+                file_name = '{}.{}'.format(str(uuid.uuid4()), extension.strip('.'))
+                file_path = self._temp_dir / file_name
 
-        # turn off remaining axes
-        #todo proplot issue
-        axes.format(xscale='log', xlabel=f'Time' + t_unit, ylabel='Corrected D-uptake', xformatter='log')
-        xlim = axes[0].get_xlim()
-        for ax in axes_iter:
-            #ax.axis('off')
-            ax.set_axis_off()
-        axes.format(xlim=xlim)
+                fig_func = partial(_peptide_uptake_figure, fig_factory, chunk, time[0], d_calc_s, hdxm)
+                self.figure_queue.append((file_path, fig_func))
 
-        return fig
+                tex_func = partial(_place_figure, file_path)
+                tex.append(tex_func)
 
-    def _make_peptide_graph(self, index, figsize=(4,4), ax_scale='log', **fig_kwargs):
-        """yield single peptide grpahs"""
+            self.tex_dict['peptide_uptake'][name] = tex
 
-        fig, ax = plt.subplots(figsize=figsize)
-        if ax_scale == 'log':
-            ax.set_xscale('log')
-            ax.get_xaxis().get_major_formatter().set_scientific(True)
-
-        ax.plot(self.fit_timepoints, self.d_calc[index], color='r')
-        ax.scatter(self.fit_result.data_obj.timepoints, self.fit_result.data_obj.d_exp[index], color='k')
-
-        t_unit = fig_kwargs.get('time_unit', 'min')
-        t_unit = f'({t_unit})' if t_unit else t_unit
-        ax.set_xlabel(f'Time' + t_unit)
-        ax.set_ylabel('Corrected D-uptake')
-        start = self.fit_result.data_obj.coverage.data['_start'][index]
-        end = self.fit_result.data_obj.coverage.data['_end'][index]
-        ax.set_title(f'peptide_{start}_{end}')
-
-
-        #ax.legend()
-        plt.tight_layout()
-
-        return fig
-
-    def _make_coverage_graph(self, index, figsize=(14, 4), cbar=True, **fig_kwargs):
-        raise NotImplementedError("coverage not implemented")
-        peptides = self.series[index]
-
-        cmap = fig_kwargs.get('cmap', 'jet')
-        if cbar:
-            fig, (ax_main, ax_cbar) = plt.subplots(1, 2, figsize=figsize, gridspec_kw={'width_ratios': [40, 1], 'wspace': 0.025})
-
-            norm = mpl.colors.Normalize(vmin=0, vmax=100)
-            cmap = mpl.cm.get_cmap(cmap)
-            cb1 = mpl.colorbar.ColorbarBase(ax_cbar, cmap=mpl.cm.get_cmap(cmap),
-                                            norm=norm,
-                                            orientation='vertical', ticks=[0, 100])
-            cb1.set_label('Uptake %', x=-1, rotation=270)
-            # cbar_ax.xaxis.set_ticks_position('top')
-            cb1.set_ticks([0, 100])
-
+    def generate_latex(self, sort_by='graphs'):  # graphs = []  #todo allow for setting which graphs to output
+        if sort_by == 'graphs':
+            for graph_type, state_dict in self.tex_dict.items():
+                #todo map graph type to human readable section name
+                with self.doc.create(pyl.Section(graph_type)):
+                    for state, tex_list in state_dict.items():
+                        with self.doc.create(pyl.Subsection(state)):
+                            [tex_func(doc=self.doc) for tex_func in tex_list]
         else:
-            fig, ax_main = plt.subplots(figsize=figsize)
+            raise NotImplementedError('Sorting by protein state not implemented')
 
-        wrap = autowrap(peptides)
-        plot_peptides(peptides, wrap, ax_main, **fig_kwargs)
-        ax_main.set_xlabel('Residue number')
-        t_unit = fig_kwargs.get('time_unit', '')
-        fig.suptitle(f'Deuterium uptake at t={peptides.exposure} ' + t_unit)
-        plt.tight_layout()
+    def generate_figures(self, executor='process'):
+        if isinstance(executor, futures.Executor):
+            exec_klass = executor
+        elif executor == 'process':
+            exec_klass = futures.ProcessPoolExecutor()
+        elif executor == 'local':
+            exec_klass = LocalThreadExecutor()
+        else:
+            raise ValueError("Invalid value for 'executor'")
 
-        return fig
+        total = len(self.figure_queue)
+        ft = [exec_klass.submit(run, item) for item in self.figure_queue]
+        with tqdm(total=total, desc='Generating figures') as pbar:
+            for future in futures.as_completed(ft):
+                pbar.update(1)
+
+    def generate_pdf(self, file_path, cleanup=True, **kwargs):
+        defaults = {'compiler_args': ['--xelatex']}
+        defaults.update(kwargs)
+
+        self.doc.generate_pdf(file_path, **defaults)
+
+        if cleanup:
+            #try:
+            self._temp_dir.clean()
+            #except:
+
+
+
+def _place_figure(file_path, width=r'\textwidth', doc=None):
+    with doc.create(pyl.Figure(position='H')) as tex_fig:
+        tex_fig.add_image(str(file_path), width=pyl.NoEscape(width))
+
+
+def _peptide_uptake_figure(fig_factory, indices, _t, _d, hdxm):
+    fig, axes = fig_factory()
+    axes_iter = iter(axes)  # isnt this alreay iterable?
+    for i in indices:
+        ax = next(axes_iter)
+        ax.plot(_t, _d[i], color='r')
+        ax.scatter(hdxm.timepoints, hdxm.d_exp.iloc[i], color='k')
+
+        start, end = hdxm.coverage.data.iloc[i][['_start', '_end']]
+        ax.format(title=f'Peptide_{i}: {start} - {end}')
+
+    for ax in axes_iter:
+        ax.axis('off')
+    # todo second y axis with RFU
+    axes.format(xscale='log', xlabel='Time (s)', ylabel='Corrected D-uptake', xformatter='log', ylim=(0, None))
+
+    return fig
+
+
+def run(item):
+    file_path, fig_func = item
+    fig = fig_func()
+    fig.savefig(file_path)
+    plt.close(fig)
+
+
+class LocalThreadExecutor(futures.Executor):
+
+    def submit(self, f, *args, **kwargs):
+        future = futures.Future()
+        future.set_result(f(*args, **kwargs))
+        return future
+
+    def shutdown(self, wait=True):
+        pass
