f58d1640b2c79d4698f6e8e6eac4533a4e495d80
Jochem Smit <jhsmit@gmail.com>
2021-10-18 11:49:19 +0200

    add script to generate example fit result for docs

---
+++ tests/gen_docs_example_result.py
@@ -0,0 +1,23 @@
+"""Obtain ΔG for ecSecB tetramer and dimer"""
+from pathlib import Path
+from pyhdx.batch_processing import yaml_to_hdxmset
+from pyhdx.fileIO import csv_to_dataframe, save_fitresult
+from pyhdx.fitting import fit_gibbs_global_batch
+import yaml
+
+cwd = Path(__file__).parent
+
+data_dir = cwd / 'test_data' / 'input'
+output_dir = cwd / 'test_data' / 'output'
+
+yaml_dict = yaml.safe_load(Path(data_dir / 'data_states.yaml').read_text())
+
+hdx_set = yaml_to_hdxmset(yaml_dict, data_dir=data_dir)
+
+initial_guess_rates = csv_to_dataframe(output_dir / 'ecSecB_guess.csv')
+
+guesses = hdx_set.guess_deltaG([initial_guess_rates['rate']]*2)
+fit_kwargs = yaml.safe_load(Path(data_dir / 'fit_settings.yaml').read_text())
+
+fr = fit_gibbs_global_batch(hdx_set, guesses,  **fit_kwargs)
+save_fitresult(output_dir / 'ecsecb_tetramer_dimer', fr)
