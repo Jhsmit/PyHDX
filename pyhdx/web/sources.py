aa3d311fc552474565ad017ceeccba11f462851d
Jochem Smit <jhsmit@gmail.com>
2021-10-31 15:41:33 +0100

    add rates results

--- pyhdx/web/sources.py
+++ pyhdx/web/sources.py
@@ -3,6 +3,7 @@ from bokeh.models import ColumnDataSource
 import numpy as np
 
 from pyhdx import TorchFitResult
+from pyhdx.fitting import GenericFitResult, KineticsFitResult, RatesFitResult
 from pyhdx.models import Protein, HDXMeasurement
 import pandas as pd
 from lumen.util import get_dataframe_schema
@@ -10,6 +11,8 @@ from lumen.util import get_dataframe_schema
 #todo refactor module to models?
 
 from lumen.sources import Source, cached_schema
+
+from pyhdx.plot import default_cmap_norm, CMAP_DEFAULTS
 
 
 class WebSource(Source):
@@ -56,7 +59,7 @@ class PyHDXSource(WebSource):
     hdxm_objects = param.Dict({})
     rate_results = param.Dict({})  # dataframes?
     dG_fits = param.Dict({})
-    cmaps = param.Dict({})   #TODO CMAP AND NORM SHOULD BE ON THE VIEWS!ONOENE
+    cmaps = param.Dict(default=CMAP_DEFAULTS)
 
     def from_file(self):
         pass
@@ -70,15 +73,23 @@ class PyHDXSource(WebSource):
         elif isinstance(obj, TorchFitResult):
             self.dG_fits[name] = obj
             self.param.trigger('dG_fits')
+        elif isinstance(obj, RatesFitResult):
+            self.rate_results[name] = obj
+            self.param.trigger('rate_results')
         else:
             raise ValueError(f"Unsupported object {obj!r}")
 
     @param.depends('hdxm_objects', watch=True)
     def _hdxm_objects_updated(self):
-        #todo perhaps some logic which takes differences such that we dont have to redo the whole thing for each addition?
         combined = pd.concat([hdxm.data for hdxm in self.hdxm_objects.values()], axis=1,
                              keys=self.hdxm_objects.keys(), names=['state', 'quantity'])  #todo 'state' or 'name' or 'protein_state'?
-        self.tables['peptides'] = combined
+        pivoted = combined \
+            .stack(level=0) \
+            .pivot(index='id', columns=['state', 'exposure']) \
+            .reorder_levels(['state', 'exposure', 'quantity'], axis=1) \
+            .sort_index(axis=1)
+
+        self.tables['peptides'] = pivoted
 
         #RFU per residue per exposure
         dfs = [hdxm.rfu_residues for hdxm in self.hdxm_objects.values()]
@@ -86,13 +97,19 @@ class PyHDXSource(WebSource):
         self.tables['rfu_residues'] = combined
 
     @param.depends('dG_fits', watch=True)
-    def _fit_results_updated(self):
+    def _fit_results_updated(self):  #todo method name / result dicts names
         combined = pd.concat([fit_result.output for fit_result in self.dG_fits.values()], axis=1,
                              keys=self.dG_fits.keys(), names=['fit_ID', 'state', 'quantity'])
         self.tables['dG_fits'] = combined
 
         # todo add d_exp etc
         #cached?:
+
+    @param.depends('rate_results', watch=True)
+    def _rates_results_updated(self):
+        combined = pd.concat([fit_result.output for fit_result in self.rate_results.values()], axis=1,
+                             keys=self.rate_results.keys(), names=['guess_ID', 'state', 'quantity'])
+        self.tables['rates'] = combined
 
     def get(self, table, *queries):
         df = self.tables[table]
