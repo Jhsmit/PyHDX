125a4c558507be3d834f5b55b01cc446c202bc14
Jochem Smit <jhsmit@gmail.com>
2022-01-05 16:23:09 +0100

    update template and added test file (needs implementation)

--- templates/07_load_fitresult.py
+++ templates/07_load_fitresult.py
@@ -5,30 +5,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-current_dir = Path().cwd()
-fit_result = load_fitresult(current_dir / 'output' / 'SecB_fit')
+# for PyCharm scientific
+# __file__ = Path
+
+current_dir = Path(__file__).parent
 
 
-time = np.logspace(-3, 2, num=100)
+print(current_dir.parent)
+print(current_dir.parent / 'tests' /'test_data' / 'output' / 'ecsecb_tetramer_dimer')
+fit_result = load_fitresult(current_dir.parent / 'tests' /'test_data' / 'output' / 'ecsecb_tetramer_dimer')
+
+s = 0 # index of protein state to view
+p = 20  # index of the peptide to view
+
+timepoints = fit_result.hdxm_set[s].timepoints
+timepoints = timepoints[np.nonzero(timepoints)]
+tmin = np.log10(timepoints.min())
+tmax = np.log10(timepoints.max())
+
+pad = 0.1*(tmax - tmin)
+
+time = np.logspace(tmin-pad, tmax+pad, num=100)
 
 d_calc = fit_result(time)
 d_exp = fit_result.hdxm_set.d_exp
 
-i = 20  # index of the protein to view
-
-fit_result.losses[['total_loss', 'mse_loss', 'reg_loss']].plot()
+fit_result.losses[['mse_loss', 'reg_1']].plot()
 
 fig, ax = plt.subplots()
-ax.scatter(fit_result.hdxm_set.timepoints, d_exp[i], color='k')
-ax.plot(time, d_calc[i], color='r')
+ax.scatter(fit_result.hdxm_set[s].timepoints, d_exp[s, p, :], color='k')
+ax.plot(time, d_calc[s, p, :], color='r')
 ax.set_xscale('log')
-ax.set_xlabel('Time (min)')
+ax.set_xlabel('Time (s)')
 ax.set_ylabel('Corrected D-uptake (Da)')
-ax.set_title(f'Peptide index: {i}')
+ax.set_title(f'Peptide index: {p}')
+
+output = fit_result.output
+protein_name = output.columns.unique(level=0)[0]
 
 fig, ax = plt.subplots()
 ax.set_aspect(2)
-ax.scatter(fit_result.output.index, fit_result.output['dG']*1e-3)
+ax.scatter(fit_result.output.index, fit_result.output[protein_name]['dG']*1e-3)
 ax.set_xlabel('Residue number')
 ax.set_ylabel('ΔG (kJ/mol)')
 plt.show()
---
+++ tests/test_templates.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib as mpl
+from pyhdx.support import rgb_to_hex
+import importlib
+from pathlib import Path
+
+class TestTemplates(object):
+
+    def test_templates_converions(self):
+        # import templates and run
+        template_dir = Path(__file__).parent # / something
+
+        # todo fix
+        scripts = [f.stem for f in template_dir.iterdir() if f.stem.startswith('fig') and f.suffix == '.py']
+        for s in scripts:
+            print(s)
+            # importlib.import_module(s)
