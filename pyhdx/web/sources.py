22af130fba799f61ac223c8fca36ba24160eff22
Jochem Smit <jhsmit@gmail.com>
2022-01-05 17:24:21 +0100

    peptide mse df from fit result

--- pyhdx/web/sources.py
+++ pyhdx/web/sources.py
@@ -147,20 +147,11 @@ class PyHDXSource(TableSource):
         self._add_table(df, 'loss')
 
         #Add MSE per peptide df
-        squared_errors = fit_result.get_squared_errors()
-        dfs = {}
-        for mse_sample, hdxm in zip(squared_errors, fit_result.hdxm_set):
-            peptide_data = hdxm[0].data
-            mse = np.mean(mse_sample, axis=1)
-            # Indexing of mse_sum with Np to account for zero-padding
-            passthrough_fields = ['start', 'end', 'sequence']
-            df = peptide_data[passthrough_fields].copy()
-            df['peptide_mse'] = mse[:hdxm.Np]
-            dfs[hdxm.name] = df
 
         # current bug: convert dtypes drop column names: https://github.com/pandas-dev/pandas/issues/41435
         # use before assigning column names
-        mse_df = pd.concat(dfs.values(), keys=dfs.keys(), axis=1).convert_dtypes()
+        mse_df = fit_result.get_peptide_mse().convert_dtypes()
+        #mse_df = pd.concat(dfs.values(), keys=dfs.keys(), axis=1).convert_dtypes()
         mse_df.index.name = 'peptide_id'
         tuples = [(name, *tup) for tup in mse_df.columns]
         columns = pd.MultiIndex.from_tuples(tuples, names=['fit_ID', 'state', 'quantity'])
