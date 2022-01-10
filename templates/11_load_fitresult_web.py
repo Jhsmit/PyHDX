057923aa9c2b13f28b4a4dfd3ad99ffc51ca7ba7
Jochem Smit <jhsmit@gmail.com>
2022-01-10 19:08:38 +0100

    add tempalte file for loading fit results directly into the web interface

---
+++ templates/11_load_fitresult_web.py
@@ -0,0 +1,21 @@
+"""Load a fit result from a a directory directly ino the web interface"""
+from pyhdx.fileIO import load_fitresult
+from pathlib import Path
+import numpy as np
+import matplotlib.pyplot as plt
+from pyhdx.web.apps import main_app
+from pyhdx.web.base import STATIC_DIR
+import sys
+import panel as pn
+
+current_dir = Path(__file__).parent
+fit_result = load_fitresult(current_dir.parent / 'tests' /'test_data' / 'output' / 'ecsecb_tetramer_dimer')
+
+ctrl, tmpl = main_app()
+
+src = ctrl.sources['main']
+for hdxm in fit_result.hdxm_set:
+    src.add(hdxm, hdxm.name)
+src.add(fit_result, 'fitresult_1')
+
+pn.serve(tmpl, show=True, static_dirs={'pyhdx': STATIC_DIR})
