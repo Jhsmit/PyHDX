ee653370d1964e58092e75ea24c879f81145a621
Jochem Smit <jhsmit@gmail.com>
2022-04-20 15:31:26 +0200

    add linters

---
+++ tasks/__init__.py
@@ -0,0 +1,8 @@
+from invoke import Collection
+
+from . import test
+
+# pylint: disable=invalid-name
+# as invoke only recognizes lower case
+namespace = Collection()
+namespace.add_collection(test)
