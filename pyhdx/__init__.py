43f374271501bef5134c8b8fb80d770b399dfccc
Jochem Smit <jhsmit@gmail.com>
2023-01-02 19:12:19 +0100

    missing version string

--- pyhdx/__init__.py
+++ pyhdx/__init__.py
@@ -9,6 +9,8 @@ from pyhdx.fitting_torch import TorchFitResult, TorchFitResultSet
 from pyhdx.batch_processing import StateParser
 from pyhdx.__version__ import __version__
 
+VERSION_STRING = f"PyHDX {__version__}"
+
 try:
     from pyhdx.output import FitReport
 except ModuleNotFoundError:
