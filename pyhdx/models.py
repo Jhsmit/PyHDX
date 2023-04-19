34bbdd6026b883a2e489f65be5a94d107d562d04
Jochem Smit <jhsmit@gmail.com>
2023-04-19 17:22:52 +0200

    fix indexing deprecation

--- pyhdx/models.py
+++ pyhdx/models.py
@@ -95,7 +95,7 @@ class Coverage:
             i0, i1 = self.r_number.get_loc(start), self.r_number.get_loc(end - 1)
             # i0, i1 = np.searchsorted(self.r_number, (entry['start'], entry['end']))
             self.X[row][i0 : i1 + 1] = 1
-            self.Z[row][i0 : i1 + 1] = _exchanges[i0 : i1 + 1]
+            self.Z[row][i0 : i1 + 1] = _exchanges.iloc[i0 : i1 + 1]
         self.Z = self.Z / self.data["ex_residues"].to_numpy()[:, np.newaxis]
 
     def __len__(self) -> int:
